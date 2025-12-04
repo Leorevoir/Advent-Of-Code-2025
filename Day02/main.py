f73bf0ec8e9c07eade8d8ef1d71b57b68dc1bb21
Leorevoir <leo.quinzler@epitech.eu>
2025-12-04 12:38:32 +0100

    add: part2

--- Day02/main.py
+++ Day02/main.py
@@ -23,6 +23,16 @@ def get_ranges_from_file(filename: str) -> list[tuple[int, int]]:
     return ranges
 
 
+if len(sys.argv) != 2:
+    error(f"USAGE: python3 {sys.argv[0]} <input_file>")
+
+ranges = get_ranges_from_file(sys.argv[1])
+starts = [r[0] for r in ranges]
+ends = [r[1] for r in ranges]
+min_len = min(len(str(r[0])) for r in ranges)
+max_len = max(len(str(r[1])) for r in ranges)
+
+
 def is_in_ranges(n: int) -> bool:
     i = bisect.bisect_right(starts, n) - 1
     if i < 0:
@@ -30,32 +40,53 @@ def is_in_ranges(n: int) -> bool:
     return n <= ends[i]
 
 
-if len(sys.argv) != 2:
-    error(f"USAGE: python3 {sys.argv[0]} <input_file>")
+def part_1() -> int:
+    acc = 0
 
-ranges = get_ranges_from_file(sys.argv[1])
-starts = [r[0] for r in ranges]
-ends = [r[1] for r in ranges]
+    for line in range(min_len, max_len + 1):
+
+        if line % 2 == 1:
+            continue
+
+        k = line // 2
+
+        start = 10 ** (k - 1)
+        end = 10**k
+
+        for motif in range(start, end):
+            s = str(motif)
+            candidate = int(s + s)
+            if is_in_ranges(candidate):
+                acc += candidate
+
+    return acc
 
 
-invalid_sum = 0
+def part_2() -> int:
 
-min_len = min(len(str(r[0])) for r in ranges)
-max_len = max(len(str(r[1])) for r in ranges)
+    acc = 0
+    seen = set()
 
-for line in range(min_len, max_len + 1):
+    for line in range(min_len, max_len + 1):
 
-    if line % 2 == 1:
-        continue
+        divisors = [k for k in range(1, line) if line % k == 0 and line // k >= 2]
 
-    half = line // 2
-    start = 10 ** (half - 1)
-    end = 10**half
+        for k in divisors:
+            m = line // k
 
-    for x in range(start, end):
-        s = str(x)
-        candidate = int(s + s)
-        if is_in_ranges(candidate):
-            invalid_sum += candidate
+            start = 10 ** (k - 1)
+            end = 10**k
 
-print(invalid_sum)
+            for motif_num in range(start, end):
+                motif = str(motif_num)
+                candidate = int(motif * m)
+
+                if candidate not in seen and is_in_ranges(candidate):
+                    acc += candidate
+                    seen.add(candidate)
+
+    return acc
+
+
+print(part_1())
+print(part_2())
