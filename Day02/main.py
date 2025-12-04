5da65190378d6d2e929f170444b55ccf81d0c30d
Leorevoir <leo.quinzler@epitech.eu>
2025-12-04 12:30:17 +0100

    feat: faster algorithm

--- Day02/main.py
+++ Day02/main.py
@@ -1,28 +1,61 @@
 import sys
+import bisect
 
 
-def error(msg: str):
+def error(msg: str) -> None:
     print(msg, file=sys.stderr)
     exit(84)
 
 
-if sys.argv.__len__() < 2:
-    error(f"USAGE: {sys.argv[0]} <input file>")
+def get_file_content(filename: str) -> str:
+    with open(filename, "r") as f:
+        return f.readline().strip()
 
-with open(sys.argv[1], "r") as stream:
-    lines = stream.readline().strip()
-    stream.close()
 
-ranges = lines.split(",")
-invalid_ids = []
-for r in ranges:
-    start, end = map(int, r.split("-"))
-    for num in range(start, end + 1):
-        s = str(num)
-        length = len(s)
-        if length % 2 == 0:
-            half = length // 2
-            if s[:half] == s[half:]:
-                invalid_ids.append(num)
+def get_ranges_from_file(filename: str) -> list[tuple[int, int]]:
+    content = get_file_content(filename)
+    ranges = []
+    for part in content.split(","):
+        start, end = map(int, part.split("-"))
+        ranges.append((start, end))
 
-print(sum(invalid_ids))
+    ranges.sort()
+    return ranges
+
+
+def is_in_ranges(n: int) -> bool:
+    i = bisect.bisect_right(starts, n) - 1
+    if i < 0:
+        return False
+    return n <= ends[i]
+
+
+if len(sys.argv) != 2:
+    error(f"USAGE: python3 {sys.argv[0]} <input_file>")
+
+ranges = get_ranges_from_file(sys.argv[1])
+starts = [r[0] for r in ranges]
+ends = [r[1] for r in ranges]
+
+
+invalid_sum = 0
+
+min_len = min(len(str(r[0])) for r in ranges)
+max_len = max(len(str(r[1])) for r in ranges)
+
+for line in range(min_len, max_len + 1):
+
+    if line % 2 == 1:
+        continue
+
+    half = line // 2
+    start = 10 ** (half - 1)
+    end = 10**half
+
+    for x in range(start, end):
+        s = str(x)
+        candidate = int(s + s)
+        if is_in_ranges(candidate):
+            invalid_sum += candidate
+
+print(invalid_sum)
